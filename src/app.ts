0a349ff62acd4af05654857f8fb89409a0ee65b9
Davi P. Oliveira <dpo.dev11@gmail.com>
2026-08-27 04:19:56 -0300

    chore: initialize project

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import Fastify from "fastify";
+import userRoutes from "./modules/user/user.route.js";
+
+const server = Fastify();
+
+server.get("/healthcheck", async function () {
+  return { status: "OK"};    
+})
+
+async function main() {
+
+   server.register(userRoutes, {prefix: 'api/users'})
+
+   try {
+     await server.listen(3000, '0.0.0.0');
+     
+     console.log(`Server ready at http://localhost:3000`);
+   } catch (e) {
+      console.error(e);
+      process.exit(1);
+   }   
+}
+
+main();
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,5 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+
+export async function registerUserHandler(request: FastifyRequest, reply: FastifyReply) {
+      
+}
